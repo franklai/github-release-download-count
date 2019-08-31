45a98c59a0cf31fbdb02c7416392563219c19cbb
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2019-08-31 18:06:37 +0800

    Updates

---
+++ precache-manifest.57e880dbd4ec85e90d80e04a4f0eccc7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623cf8b519d82e6edde670a6e232c98b",
+    "url": "/github-release-download-count/index.html"
+  },
+  {
+    "revision": "8bbfaa4f0d58bebf908f",
+    "url": "/github-release-download-count/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "67c005b9d261cd0c7dd1",
+    "url": "/github-release-download-count/static/js/2.db7de938.chunk.js"
+  },
+  {
+    "revision": "8bbfaa4f0d58bebf908f",
+    "url": "/github-release-download-count/static/js/main.3bf3877e.chunk.js"
+  },
+  {
+    "revision": "a46534ce6dd66fd872f0",
+    "url": "/github-release-download-count/static/js/runtime~main.7fafde59.js"
+  }
+]);
